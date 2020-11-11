d4a3d43ca2aea871a9fc9cfa6290fbf43a257df9
SD6399 <david-shapiro@mail.ru>
2020-11-11 22:09:10 +0400

    изменены регулярные выражения

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import re
 file1 = open("1.txt", "r")
 
 for line in file1:
-    if ((re.findall(r'\d\d\d\d-\d\d\d-\d\d-\d\d\n', line) or re.findall(r'\d[(]\d\d\d[)]\d\d\d-\d\d-\d\d\n', line))
-        and (line[0] == "8")) or ((re.findall(r'\d\d\d\d\d\d\d\d\d\d\d\n', line)) and (line[0] == "8")
-                                  or (line[0] == "7"))\
-            or ((re.findall(r'\d\d\d\d\d\d\d\d\d\d\n', line)) and (line[0] == "9")):
+    if (re.findall(r'8\d\d\d-\d\d\d-\d\d-\d\d\n', line) or re.findall(r'8[(]\d\d\d[)]\d\d\d-\d\d-\d\d\n', line)) \
+            or ((re.findall(r'\d\d\d\d\d\d\d\d\d\d\d\n', line)) and (line[0] == "8")
+                or (line[0] == "7")) \
+            or (re.findall(r'9\d\d\d\d\d\d\d\d\d\n', line)):
         print(line)
 file1.close()
